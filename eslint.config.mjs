c8921a676191e9d2f8b8f69e6f36075e0f654623
Otávio Ferreira <ottvferreira@gmail.com>
2026-03-23 14:29:23 -0300

    chore: initialize monorepo with pnpm workspaces

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: { parser: tsParser },
+    plugins: { '@typescript-eslint': tsPlugin },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-console': 'warn',
+    },
+  },
+]
